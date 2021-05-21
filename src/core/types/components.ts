6a8bd85eab677e4bd03aa290adbefc7eb1903f9c
anclaev <iahugo@yandex.ru>
2021-05-21 16:39:06 +0400

    Компонент кастомных стилей.

--- src/core/types/components.ts
+++ src/core/types/components.ts
@@ -1,7 +1,14 @@
+import { FlattenSimpleInterpolation } from 'styled-components'
+
 export interface IBackground {
   opacity?: string
   isPoster?: boolean
   autoplay?: boolean
   loop?: boolean
   muted?: boolean
+}
+
+export interface ICustomStyle {
+  child: React.FC
+  css: FlattenSimpleInterpolation
 }
